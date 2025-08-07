3e34e2c370d300bde4faeac765a65369bc0e63ba
Ratakonda_sushma <sushmaratakonda514@gmil.com>
2025-08-07 18:58:15 +0530

    changes

--- Day-4-statelocking/backend.tf
+++ Day-4-statelocking/backend.tf
@@ -4,5 +4,7 @@ terraform {
     key = "day-4/terraform.tfstate"
     region = "us-east-1"
     use_lockfile = "true" #s3 supports this feature but terraform latest version >1.10
+    dynamodb_table = "test1"
+    encrypt = true
   }
 }
