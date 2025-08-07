deda807d652b8b5a25274aa4454b0244132aa303
Ratakonda_sushma <sushmaratakonda514@gmil.com>
2025-08-07 18:35:55 +0530

    changes

--- Day-4-statelocking/backend.tf
+++ Day-4-statelocking/backend.tf
@@ -3,5 +3,6 @@ terraform {
     bucket = "vikkykannayyasush321"
     key = "day-4/terraform.tfstate"
     region = "us-east-1"
+    use_lockfile = "true" #s3 supports this feature but terraform latest version >1.10
   }
 }
